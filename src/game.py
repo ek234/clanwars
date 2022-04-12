449ae11d0f4e7cd438af26d5dfee8d8d4f36aeaa
ek234 <71319388+ek234@users.noreply.github.com>
2022-04-12 05:51:46 +0530

    integrated multiple levels

--- src/game.py
+++ src/game.py
@@ -2,7 +2,7 @@ import os
 import time
 import pickle
 from components import game
-from stage1 import spawns, townhalls_at, huts_at, cannons_at, towers_at, walls_at
+import stage1, stage2, stage3
 from utils import CHANGE_WEAPON
 from utils import UP, RIGHT, DOWN, LEFT
 from utils import SPELL_RAGE, SPELL_HEAL, SPELL_RISE
@@ -15,72 +15,85 @@ from endpage import ending
 # TODO : add readme
 
 timePerFrame = 1/game.fps
-seed = time.time()
+stages = [ stage1, stage2, stage3 ]
 filename = str(time.time())+".rpl"
 saveData = {}
-saveData["moves"] = {}
-
-game.gameInit( spawns, townhalls_at, huts_at, cannons_at, towers_at, walls_at, seed )
-saveData["seed"] = seed
 
 isKing = None
 while isKing not in { "k", "q" } :
     isKing = input("press k for king and q for queen : ")
 isKing = isKing == "k"
 saveData["isKing"] = isKing
-inp = None
-while inp not in { 0, 1, 2 } :
-    try :
-        inp = int(input("spawn player at 1, 2 or 3: ")) - 1
-    except :
-        inp = None
-gameState = game.gameStart(inp, isKing)
-saveData["playerSpawn"] = inp
 
-os.system("stty -echo")
+gameState = NOTSTARTED
 
-ite = 0
-prevFrameTime = time.time()
-while gameState is INGAME :
-    ite+=1
+for stageID, stage in enumerate(stages) :
+    saveData[stageID] = {}
+
+    seed = time.time()
+    saveData[stageID]["seed"] = seed
+
+    game.gameInit( stage.spawns, stage.townhalls_at, stage.huts_at, stage.cannons_at, stage.towers_at, stage.walls_at, seed )
+
+    saveData[stageID]["moves"] = {}
 
     inp = None
-    waitTime = timePerFrame - ( time.time() - prevFrameTime )
-    if waitTime > 0 :
-        inp = input_to(Get(), waitTime)
-    waitTime = timePerFrame - ( time.time() - prevFrameTime )
-    if waitTime > 0 :
-        time.sleep(waitTime)
+    while inp not in { 0, 1, 2 } :
+        try :
+            inp = int(input("spawn player at 1, 2 or 3: ")) - 1
+        except :
+            inp = None
+    gameState = game.gameStart(inp, isKing)
+    saveData[stageID]["playerSpawn"] = inp
+
+    os.system("stty -echo")
+
+    ite = 0
     prevFrameTime = time.time()
+    while gameState is INGAME :
 
-    if inp != None :
-        saveData["moves"][ite] = inp
+        ite+=1
 
-    if inp == " " :
-        if game.player != None :
-            game.player.attack()
-    elif inp == CHANGE_WEAPON :
-        if game.player != None :
-            game.player.switchWeapon()
-    elif inp in { UP, LEFT, DOWN, RIGHT } :
-        if game.player != None :
-            game.player.move(inp, timePerFrame)
-    elif inp in {'1','2','3', '4', '5', '6', '7', '8', '9'} :
-        game.spawn(int(inp)-1)
-    elif inp == SPELL_HEAL :
-        game.spell_heal()
-    elif inp == SPELL_RAGE :
-        game.spell_rage()
-    elif inp == SPELL_RISE :
-        game.spell_rise()
-    elif inp == '/' :
+        inp = None
+        waitTime = timePerFrame - ( time.time() - prevFrameTime )
+        if waitTime > 0 :
+            inp = input_to(Get(), waitTime)
+        waitTime = timePerFrame - ( time.time() - prevFrameTime )
+        if waitTime > 0 :
+            time.sleep(waitTime)
+        prevFrameTime = time.time()
+
+        if inp != None :
+            saveData[stageID]["moves"][ite] = inp
+
+        if inp == " " :
+            if game.player != None :
+                game.player.attack()
+        elif inp == CHANGE_WEAPON :
+            if game.player != None :
+                game.player.switchWeapon()
+        elif inp in { UP, LEFT, DOWN, RIGHT } :
+            if game.player != None :
+                game.player.move(inp, timePerFrame)
+        elif inp in {'1','2','3', '4', '5', '6', '7', '8', '9'} :
+            game.spawn(int(inp)-1)
+        elif inp == SPELL_HEAL :
+            game.spell_heal()
+        elif inp == SPELL_RAGE :
+            game.spell_rage()
+        elif inp == SPELL_RISE :
+            game.spell_rise()
+        elif inp == '/' :
+            break
+
+        gameState = game.gameloop(timePerFrame, ite)
+
+    ending( gameState == WON )
+
+    if gameState != WON :
         break
 
-    gameState = game.gameloop(timePerFrame, ite)
-
-saveData["num_ite"] = ite
-
-ending( gameState == WON )
+    saveData[stageID]["num_ite"] = ite
 
 print("saving data in", filename)
 os.system("mkdir -p replays")
--- src/replay.py
+++ src/replay.py
@@ -2,15 +2,18 @@ import os
 import time
 import pickle
 from components import game
-from stage1 import spawns, townhalls_at, huts_at, cannons_at, towers_at, walls_at
+import stage1, stage2, stage3
 from utils import CHANGE_WEAPON
 from utils import UP, RIGHT, DOWN, LEFT
 from utils import SPELL_RAGE, SPELL_HEAL, SPELL_RISE
 from utils import NOTSTARTED, INGAME, WON, LOST
+from getinput import Get, input_to
 from endpage import ending
 
 timePerFrame = 1/game.fps
+stages = [ stage1, stage2, stage3 ]
 saveData = {}
+
 while True :
     filename = input("enter filename: ")
     try :
@@ -20,52 +23,61 @@ while True :
         print("please enter correct filename")
     else :
         break
-os.system("stty -echo")
-
-game.gameInit( spawns, townhalls_at, huts_at, cannons_at, towers_at, walls_at, saveData["seed"] )
 
 isKing = saveData["isKing"]
 
-gameState = game.gameStart(saveData["playerSpawn"], isKing)
+gameState = NOTSTARTED
 
-ite = 0
-prevFrameTime = time.time()
-while gameState is INGAME :
-    ite+=1
+for stageID, stage in enumerate(stages) :
 
-    inp = None
-    waitTime = timePerFrame - ( time.time() - prevFrameTime )
-    if waitTime > 0 :
-        time.sleep(waitTime)
+    seed = saveData[stageID]["seed"]
+
+    game.gameInit( stage.spawns, stage.townhalls_at, stage.huts_at, stage.cannons_at, stage.towers_at, stage.walls_at, seed )
+
+    playerSpawn = saveData[stageID]["playerSpawn"]
+    gameState = game.gameStart(playerSpawn, isKing)
+
+    os.system("stty -echo")
+
+    ite = 0
     prevFrameTime = time.time()
+    while gameState is INGAME :
+        ite+=1
 
-    if ite in saveData["moves"] :
-        inp = saveData["moves"][ite]
+        inp = None
+        waitTime = timePerFrame - ( time.time() - prevFrameTime )
+        if waitTime > 0 :
+            time.sleep(waitTime)
+        prevFrameTime = time.time()
 
-    if inp == " " :
-        if game.player != None :
-            game.player.attack()
-    elif inp == CHANGE_WEAPON :
-        if game.player != None :
-            game.player.switchWeapon()
-    elif inp in { UP, LEFT, DOWN, RIGHT } :
-        if game.player != None :
-            game.player.move(inp, timePerFrame)
-    elif inp in {'1','2','3', '4', '5', '6', '7', '8', '9'} :
-        assert inp != None
-        game.spawn(int(inp)-1)
-    elif inp == SPELL_HEAL :
-        game.spell_heal()
-    elif inp == SPELL_RAGE :
-        game.spell_rage()
-    elif inp == SPELL_RISE :
-        game.spell_rise()
-    elif inp == '/' :
+        if ite in saveData[stageID]["moves"] :
+            inp = saveData[stageID]["moves"][ite]
+
+        if inp == " " :
+            if game.player != None :
+                game.player.attack()
+        elif inp == CHANGE_WEAPON :
+            if game.player != None :
+                game.player.switchWeapon()
+        elif inp in { UP, LEFT, DOWN, RIGHT } :
+            if game.player != None :
+                game.player.move(inp, timePerFrame)
+        elif inp in {'1','2','3', '4', '5', '6', '7', '8', '9'} :
+            game.spawn(int(inp)-1)
+        elif inp == SPELL_HEAL :
+            game.spell_heal()
+        elif inp == SPELL_RAGE :
+            game.spell_rage()
+        elif inp == SPELL_RISE :
+            game.spell_rise()
+        elif inp == '/' :
+            break
+
+        gameState = game.gameloop(timePerFrame, ite)
+
+    ending( gameState == WON )
+
+    if gameState != WON :
         break
 
-    gameState = game.gameloop(timePerFrame, ite)
-
-    if ite > saveData["num_ite"] :
-        break
-
-ending( gameState == WON )
+    saveData[stageID]["num_ite"] = ite
